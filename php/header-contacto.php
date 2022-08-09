894c44ccdec0bc212bdb46443c7137af32efca61
DaniPineda7687 <85198473+DaniPineda7687@users.noreply.github.com>
2022-08-08 20:41:11 -0500

    Delete php directory

--- php/header-contacto.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <div class="logo-container">
-        <a href="../index.php">
-            <img src="https://gruposanar.com.co/imagenes/logo5.png" alt="logo-ips-grupo-sanar" srcset="">
-        </a>
-    </div>
-</header>
--- php/whatsapp-section.php
+++
@@ -1,3 +0,0 @@
-<div class="wpp-container">
-    <a href=""><i class="bi bi-whatsapp"></i></a>
-</div>
